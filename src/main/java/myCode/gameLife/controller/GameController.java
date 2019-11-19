9d1f6015643ae6f91b5adc2f878da5c61dae147b
Арсен <arsennaimov@gmail.com>
2019-11-19 04:03:24 +0300

    Add Cross-Origin Resource Sharing

--- src/main/java/myCode/gameLife/controller/GameController.java
+++ src/main/java/myCode/gameLife/controller/GameController.java
@@ -4,6 +4,7 @@ import myCode.gameLife.entity.Game;
 import myCode.gameLife.service.GameService;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/game")
 public class GameController {
